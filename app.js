62ae773188bff1268d8c1181b82b748bf94e5620
Maneva <manevarsld@gmail.com>
2022-05-13 17:46:21 +0200

    premier test et configuration fait

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var express = require('express');
 
+var bodyparser = require('body-parser');
+
+var app=express();
+app.use(bodyparser.json());
+
+app.get('/',function(req,res){
+    res.send('Ca marche!');
+})
+app.listen(3000,function(){
+
+    console.log("Server running..");
+});
